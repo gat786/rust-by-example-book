564f50fb608883e8f11c9af4ac96812914f3ef8e
Ganesh Tiwari <ganesht049@gmail.com>
2024-03-24 11:43:37 +0530

    feat: added enum code

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
@@ -20,8 +22,62 @@ enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    New_Hampshire,
+    New_Jersey,
+    New_Mexico,
+    New_York,
+    North_Carolina,
+    North_Dakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    Rhode_Island,
+    South_Carolina,
+    South_Dakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    West_Virginia,
+    Wisconsin,
+    Wyoming,
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -32,7 +88,17 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
@@ -56,4 +122,16 @@ fn main() {
     println!("HomeVal is {:?}", home_val);
 
     println!("Value of a penny is {}", value_in_cents(Coin::Nickel));
+    println!("Value of a quarter is {}", value_in_cents(Coin::Quarter(UsState::Wyoming)));
+
+    let some_number : Option<i8> = Some(5);
+    let some_other_number = 10;
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("Value of five is {:?}", five);
+    println!("Value of six is {:?}", six);
+    println!("Value of none is {:?}", none);
 }
